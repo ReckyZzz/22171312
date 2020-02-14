75b4ddbbf9a96f675f9c12ea612ec098742d88d0
ReckyZzz <244373762@qq.com>
2020-02-14 15:02:09 +0800

    Update InfectStatistic.java
    定义好3个大致类

--- example/src/InfectStatistic.java
+++ example/src/InfectStatistic.java
@@ -7,7 +7,25 @@
  * @since xxx
  */
 class InfectStatistic {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
+    public static void main(String[] args)
+    {
+        CommandIdentity cmdit=new CommandIdentity(args);
+        cmdit.PrintCommand();
+    }
+}
+class FileManager{
+    public FileManager(){
+
+    }
+}
+class CommandIdentity{
+    public String[] command;
+    public CommandIdentify(String[] x){
+        command = x;
+    }
+    public void PrintCommand(){
+        for(String s:command){
+            System.out.print(s);
+        }
     }
 }
